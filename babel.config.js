6ef2281ba77fc0babe068adc9401c46bc9a61474
ilyeongjeong <developjiy@gmail.com>
2020-09-23 17:30:18 +0900

    ref #1 root babel 설정

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = function(api) {
+  api.cache(true);
+
+  const presets = [
+    "@babel/preset-env"
+  ];
+
+  const plugins = [
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-export-default-from",
+    "@babel/plugin-syntax-dynamic-import"
+  ];
+
+  return {
+    presets,
+    plugins
+  };
+}
