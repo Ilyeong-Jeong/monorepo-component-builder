a77a7363526f02b5a5079a58fb317d56ef6bda04
ilyeongjeong <developjiy@gmail.com>
2020-10-05 17:29:40 +0900

    ref #1 main 환경 테스트

--- packages/main/src/app.vue
+++ packages/main/src/app.vue
@@ -1,15 +1,40 @@
 <style lang="scss">
 
+  .default-layout {
+    width           : 100%;
+    background-color: grey;
+    color           : blue;
+    font-weight     : bold;
+
+    .default-layout-sub {
+      width           : 100%;
+      background-color: green;
+      color           : red;
+    }
+  }
+
 </style>
 
 <template>
+  <div class="default-layout">
+    {{text}}
 
+    <div class="default-layout-sub">
+      {{text}}
+    </div>
+  </div>
 </template>
 
 <script>
 
   export default {
-
+    
+    data () {
+      return {
+        text: "main"
+      }
+    }
+    
   }
-  
+
 </script>
